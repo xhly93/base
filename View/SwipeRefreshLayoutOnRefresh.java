db5337eed62b6f2cc690cdba19eede8a51356f31
xhly93 <1725837808@qq.com>
2018-05-11 13:54:35 +0800

    add util

---
+++ View/SwipeRefreshLayoutOnRefresh.java
@@ -0,0 +1,22 @@
+package sharing.school.ctq.wiget;
+
+import android.support.v4.widget.SwipeRefreshLayout;
+
+/**
+ *
+ */
+public class SwipeRefreshLayoutOnRefresh implements SwipeRefreshLayout.OnRefreshListener {
+    private PullLoadMoreRecyclerView mPullLoadMoreRecyclerView;
+
+    public SwipeRefreshLayoutOnRefresh(PullLoadMoreRecyclerView pullLoadMoreRecyclerView) {
+        this.mPullLoadMoreRecyclerView = pullLoadMoreRecyclerView;
+    }
+
+    @Override
+    public void onRefresh() {
+        if (!mPullLoadMoreRecyclerView.isRefresh()) {
+            mPullLoadMoreRecyclerView.setIsRefresh(true);
+            mPullLoadMoreRecyclerView.refresh();
+        }
+    }
+}
